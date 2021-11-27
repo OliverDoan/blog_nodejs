3abf68801b7c403606fee9139180b86bcc99a33c
OliverDoan <doanvietthuan1005@gmail.com>
2021-11-27 10:36:35 +0700

    create Route, Controller

--- src/index.js
+++ src/index.js
@@ -5,21 +5,25 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const hbs = exphbs.create( { extname: '.hbs'})
 const path = require('path'); 
-
+const route = require('./routes');
 
 
 // Use static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+  express.urlencoded({
+      extended: true,
+  }),
+);
+app.use(express.json());
 
 app.engine('hbs', hbs.engine )
 app.set('view engine', 'hbs');
 app.set("views", path.join(__dirname, 'resources/views'));
 
-
-app.get('/', (req, res) => {
-  res.render('home')
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
